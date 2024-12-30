176c6c8f48d6967bcf8c6ee967455e27aa1b9737
JiwanChaudhary <jeewanchaudhary6@gmail.com>
2024-12-30 14:36:40 +0545

    fix: build error

--- src/services/api/api-service/admin/product/add-product.ts
+++ src/services/api/api-service/admin/product/add-product.ts
@@ -4,13 +4,13 @@ import { api } from '@/services/endpoints/api.endpoints'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 const postAddProduct = async (data: Partial<TAddProductSchemaProps>) => {
-  return await httpClient.post(api.cart.post, data)
+  return await httpClient.post(api.admin.product.add.post, data)
 }
 
 export const usePostAddProduct = () => {
   const queryClient = useQueryClient()
   return useMutation({
-    mutationKey: ['post' + api.cart.post],
+    mutationKey: ['post' + api.admin.product.add.post],
     mutationFn: postAddProduct,
     onSuccess: () => {
       queryClient.invalidateQueries({
